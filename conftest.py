60bf35c8ecf01bf6120e4be0bf18e7310101b73b
Yury.Baranik <yurij.b@ati.su>
2020-12-04 17:49:16 +0300

    add PageObject pattern to project

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
     parser.addoption('--language',
                      action='store',
-                     default='ru',
+                     default='en',
                      help="Choose language for site"
                      )
 
