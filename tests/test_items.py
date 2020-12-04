2017d937f6f555871d5f380fada6b00ad97dd3c3
Yury.Baranik <yurij.b@ati.su>
2020-12-04 16:23:50 +0300

    add assert

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,4 +1,5 @@
 def test_page_has_button_add_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
-    browser.find_element_by_class_name('btn-add-to-basket')
+    butthon = browser.find_element_by_class_name('btn-add-to-basket')
+    assert butthon, 'Кнопка не найдена, сорян'
